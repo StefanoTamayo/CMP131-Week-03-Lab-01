0fb801185e9cddea018c7a2485d28d4b82bfa1c0
StefanoTamayo <tamayo.stefano@student.ccm.edu>
2026-09-09 11:22:28 -0400

    Create character_profile.py for Kyojuro Rengoku
    Added a character profile for Kyojuro Rengoku, including personal and quote information.

---
+++ character_profile.py
@@ -0,0 +1,32 @@
+#Stefano Tamayo
+#CMP-131-80230
+#Week: 3
+#Lab 1
+#Assignment: Character Profile
+#Date: 9/9/2026
+
+# Character Information
+name="His name, Kyojuro Rengoku"
+age="He was 20 years old"
+occupation="He was a Demon Slayer, a member of the Hashiras"
+home="His home, the Rengoku Residence"
+pet="He had a crow named Kaname"
+ability="He was a Fire Breathing Hashira"
+goal="His final mission was to defeat Upper Moon 3, Akaza"
+
+# Quote Information
+quote='"If you are feeling disheartened, that you are somehow not enough, set you heart ablaze"'
+author="-Koyoharu Gotouge"
+
+print("---ABOUT THE FLAME HASHIRA---")
+print(name)
+print(age)
+print(occupation)
+print(home)
+print(pet)
+print(ability)
+print(goal)
+print()
+print("---SOME OF RENGOKUS FINAL WORDS---")
+print(quote)
+print(author)
